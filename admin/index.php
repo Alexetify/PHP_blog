d461cf1a710a0238b102d5c6899176f7688b0b7a
ALEXEI-PC\Alexei-PC <hhmh@rambler.ru>
2017-02-02 19:06:35 +0200

    lesson11

--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,20 @@
 <?php
 	
-	echo "Admin Panel";
+	require_once("../database.php");
+    require_once("../models/articles.php");
+
+    $link = db_connect();
+    if(isset($_GET['action']))
+        $action = $_GET['action'];
+    else
+        $action ="";
+    if($action == "add"){
+        include("../views/article_admin.php");
+    } else{
+    $articles = articles_all($link);
+
+    include("../views/articles_admin.php");
+    
+    }
 
 ?>
--- models/articles.php
+++ models/articles.php
@@ -34,7 +34,28 @@
     }
 
     function articles_new($title, $date, $content) {
-
+        
+        $title = trim($title);
+        $content = trim($content);
+        
+        if ($title == '')
+            return false;
+        
+        $t ="INSERT INTO articles (title, date, content) VALUES ('%s', '%s', '%s')";
+        
+        
+        $query = sprintf($t, mysqli_real_escape_string($link, $title),
+                        mysqli_real_escape_string($link, $date),
+                         mysqli_real_escape_string($link, $content));
+        
+        echo $query;
+        $result = mysqli_query($link, $query);
+        
+        if (!$result)
+            die(mysqli_error($link));
+        
+        return true;
+        
     }
 
     function articles_edit($id, $title, $date, $content) {
